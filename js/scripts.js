71a59c9441800a035797f145db912e8e270de30c
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-04-11 13:55:21 +0100

    Added additional spacing and comments to improve readability.

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,31 @@ function newItem(){
   let inputValue = $('#input').val();
   li.append(inputValue);
 
+//  Only adding the list item if a value has been entered
   if (inputValue === '') {
      alert('Enter an item to add to your list');
   } else {
      $('#list').append(li);
   }
+
 //  To cross out a completed item from the List
   li.on('dblclick',crossOut);
 
    function crossOut() {
  		li.toggleClass('strike');
  	}
+
 //  Adding a delete button to list items to remove them
   let crossOutButton = $('<crossOutButton></crossOutButton>');
   crossOutButton.append(document.createTextNode('X'));
   li.append(crossOutButton);
+
   crossOutButton.on('click', deleteListItem);
+
   function deleteListItem(){
  		li.addClass('delete');
  	}
+
 //  Adding ability to re-order items on the List
   $('#list').sortable();
 
