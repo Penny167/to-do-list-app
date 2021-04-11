60e6e14abfedbc1b4a2f11ac197a9e0264308bc3
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-04-11 13:28:00 +0100

    Tidied indentation and added delete button to remove items from the list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
 function newItem(){
 //  To add a new item to the List
-let li = $('<li></li>');
-   let inputValue = $('#input').val();
-   li.append(inputValue);
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
 
-   if (inputValue === '') {
+  if (inputValue === '') {
      alert('Enter an item to add to your list');
-   } else {
+  } else {
      $('#list').append(li);
-   }
+  }
 //  To cross out a completed item from the List
-li.on('dblclick',crossOut);
+  li.on('dblclick',crossOut);
 
    function crossOut() {
  		li.toggleClass('strike');
+ 	}
+//  Adding a delete button to list items to remove them
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+  crossOutButton.on('click', deleteListItem);
+  function deleteListItem(){
+ 		li.addClass('delete');
  	}
